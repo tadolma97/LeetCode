1598edb186d80de5d38a4b85b21f100f292dbf00
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2022-12-21 12:47:10 -0500

    Time: 128 ms (14.67%), Space: 44.1 MB (30.52%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -12,7 +12,6 @@ var addBinary = function(a, b) {
     let sum=[]
     while (aIndex<=aNum.length-1||bIndex<=bNum.length-1||carry){
         let add= (aNum[aIndex]? aNum[aIndex] : 0) + (bNum[bIndex] ? bNum[bIndex]: 0) + carry;
-        console.log(add, "first")
         if(add>2){
             carry=1
             add=1
@@ -24,9 +23,7 @@ var addBinary = function(a, b) {
         else{
             carry=0
         }
-        console.log(aIndex, bIndex)
         sum.push(add)
-        console.log(sum,"second")
         if(aIndex<=aNum.length-1){
             aIndex++
         }
@@ -34,10 +31,7 @@ var addBinary = function(a, b) {
             bIndex++
         }
     }
-    console.log(sum,"pp")
     let correct=sum.reverse()
-    console.log(correct)
-    console.log(correct.toString(), "l")
     return correct.join('')
     
 };
