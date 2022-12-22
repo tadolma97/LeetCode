da7c255bcc43a6f990613b631066afa43dc2af46
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2022-12-22 18:49:24 -0500

    Time: 79 ms (98.21%), Space: 47.6 MB (59.23%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let max=0
+    let count=0
+    let substring=''
+    for(let i=0; i<s.length;i++){
+       if(substring.includes(s[i])){
+           const index=substring.indexOf(s[i])+1
+           substring=substring.slice(index)+s[i]
+       }
+        else{
+            substring+=s[i]
+
+        }
+        count=Math.max(count, substring.length)
+    }
+    return count
+};
