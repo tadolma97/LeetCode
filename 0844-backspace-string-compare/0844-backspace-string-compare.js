269c434a8d345ac8a2202dba8431bd1cb0834730
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-04 16:08:24 -0500

    Time: 127 ms (8.52%), Space: 44.3 MB (14.47%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    s=s.split('')
+    t=t.split('')
+    if(deleteString(s)===deleteString(t)) return true
+    console.log(s,t)
+    return false
+};
+
+var deleteString=function(s){
+    for(let i=0; i<s.length;i++){
+        if(s[i]==='#' && i===0){
+           s.splice(0,1)
+            i=-1
+        }  
+        else if(s[i]==='#') {
+            s.splice(i-1,2) 
+            i=-1
+        }
+    }
+    return s.join('')
+}
