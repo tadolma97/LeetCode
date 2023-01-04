c2a45099c2be237bf4f77dc6f4e16bb2f78bc62c
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-04 16:21:20 -0500

    Time: 267 ms (5.02%), Space: 50.7 MB (5.02%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -4,23 +4,39 @@
  * @return {boolean}
  */
 var backspaceCompare = function(s, t) {
-    s=s.split('')
-    t=t.split('')
-    if(deleteString(s)===deleteString(t)) return true
-    console.log(s,t)
+    if(returnStack(s)===returnStack(t)) return true
     return false
-};
-
-var deleteString=function(s){
-    for(let i=0; i<s.length;i++){
-        if(s[i]==='#' && i===0){
-           s.splice(0,1)
-            i=-1
-        }  
-        else if(s[i]==='#') {
-            s.splice(i-1,2) 
-            i=-1
-        }
-    }
-    return s.join('')
 }
+var returnStack=function(s){
+    let stack=[]
+    for(let i=0; i<s.length;i++){
+        console.log(s[i])
+        if(s[i]==='#') stack.pop()
+        else stack.push(s[i])
+    }
+        console.log(stack)
+    return stack.join('')
+
+}
+
+// var backspaceCompare = function(s, t) {
+//     s=s.split('')
+//     t=t.split('')
+//     if(deleteString(s)===deleteString(t)) return true
+//     console.log(s,t)
+//     return false
+// };
+
+// var deleteString=function(s){
+//     for(let i=0; i<s.length;i++){
+//         if(s[i]==='#' && i===0){
+//            s.splice(0,1)
+//             i=-1
+//         }  
+//         else if(s[i]==='#') {
+//             s.splice(i-1,2) 
+//             i=-1
+//         }
+//     }
+//     return s.join('')
+// }
