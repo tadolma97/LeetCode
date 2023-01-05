bf4e5d6b0027bf6029bfef0747a0b3dbed4501c6
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-05 12:44:34 -0500

    Time: 111 ms (9.41%), Space: 41.7 MB (89.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,11 +4,17 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if(target>nums[nums.length-1])return nums.length
-    if(target<nums[0])return 0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target) return i
-        else if (nums[i]<target&& nums[i+1]>target) return i+1
-    
+    let l=0
+    let r=nums.length-1
+    while(l<r){
+        let m=Math.floor((r+l)/2)
+        if(target===nums[m]) return m
+        else if(target<nums[m]){
+            r=m
+        }else l=m+1
+    }
+    if(l===r){
+        if(target<=nums[l]) return l
+        return l+1
     }
 };
