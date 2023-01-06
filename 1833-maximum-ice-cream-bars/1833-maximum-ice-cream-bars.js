6f1cf6209cceb76a634186db692e5f210a32d140
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-06 16:47:04 -0500

    Time: 247 ms (98.33%), Space: 56 MB (43.33%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -5,11 +5,9 @@
  */
 var maxIceCream = function(costs, coins) {
     costs.sort(function(a, b){return a-b})
-    console.log(costs)
     let answer=0
     let price=0
     for(let i=0; i<costs.length;i++){
-        console.log(costs[i], price)
         if(costs[i]<coins && costs[i]<=coins-price){
             price+=costs[i]
             answer++
