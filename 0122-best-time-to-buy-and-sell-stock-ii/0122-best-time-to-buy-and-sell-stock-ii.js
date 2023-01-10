763cabbbce909229bb71dea36f948279bb7dc0d6
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-10 12:55:57 -0500

    Time: 302 ms (5.11%), Space: 52.2 MB (5.00%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min=Infinity
+    let profit=[]
+    let maxProfit=0
+    for(let i=0;i<prices.length;i++){
+        if(prices[i]<min) min=prices[i]
+        else if(prices[i+1]<prices[i]){
+            let difference=prices[i]-min
+            profit.push(difference)
+            min=prices[i+1]
+            i=i+1
+        }else if(i===prices.length-1){
+            console.log('hi')
+            difference=prices[prices.length-1]-min
+            profit.push(difference)
+            break
+        }
+        console.log(profit,min,i)
+    }
+    if(profit.length===0){
+        maxProfit=prices[prices.length-1]-min
+    }
+    for(let i=0;i<profit.length;i++){
+        
+        maxProfit+=profit[i]
+    }
+    
+    return maxProfit
+};
