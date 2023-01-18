30ba3930e22417a9a487887448d159e9b4520bc7
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-01-18 12:59:53 -0500

    Time: 377 ms (35.54%), Space: 50.5 MB (91.71%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n<=0) return false
+    if(n===1) return true
+    else n=n/3
+    return isPowerOfThree(n)
+};
