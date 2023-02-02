9b5967a905ba33b6c6b228eced690038a3dc7fa8
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-02-02 16:46:41 -0500

    Time: 60 ms (89.91%), Space: 42.3 MB (54.58%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,11 +3,10 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let result=[]
     let number=0
    for(let i=0; i<nums.length;i++){
        number+=nums[i]
-       result.push(number)
+       nums[i]=number
    }
-    return result
+    return nums
 };
