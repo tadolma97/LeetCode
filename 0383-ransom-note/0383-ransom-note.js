0f50f98fba9c3760b31d8cde8864d1a74c3edae7
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-02-07 18:38:41 -0500

    Time: 102 ms (49.66%), Space: 44.2 MB (79.51%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -5,8 +5,7 @@
  */
 var canConstruct = function(ransomNote, magazine) {
     let object={}
-    magazine=magazine.split('')
-    ransonNote=ransomNote.split('')
+
     for(let letter of magazine){
         if(!(letter in object)){
             object[letter]=1
