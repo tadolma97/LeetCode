2d0c121238704ccec7305f3788c6c97996407a60
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-02-15 12:44:46 -0500

    Time: 68 ms (43.17%), Space: 41.9 MB (67.63%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.js
+++ 0165-compare-version-numbers/0165-compare-version-numbers.js
@@ -6,14 +6,12 @@
 var compareVersion = function(version1, version2) {
     const v1 = version1.split('.').map(num => Number(num))
     const v2 = version2.split('.').map(num => Number(num))
-        console.log(v1,v2)
     const diff = Math.abs(v1.length - v2.length);
     if(v1.length > v2.length) {
         v2.push(...new Array(diff).fill(0));
     } else {
         v1.push(...new Array(diff).fill(0));
     }
-    console.log(v1,v2)
     for(let i = 0; i < v1.length; i++) {
         if(v1[i] > v2[i]) return 1;
         else if(v1[i] < v2[i]) return -1;
