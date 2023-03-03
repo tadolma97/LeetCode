a0714977ba149b4fad52f385695eb470d8449bbb
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-03-03 14:13:55 -0500

    Time: 104 ms (51.04%), Space: 51 MB (55.82%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+    const fakeHead = new TreeNode(null, root, null)
+    let curr=root
+    let prev=fakeHead
+    if(root===key) return null
+    while(curr && curr.val!==key){
+        prev=curr
+        if(curr.val>key){
+            curr=curr.left
+        }else{
+            curr=curr.right
+        }
+    }
+    if(!curr){
+        return root;
+    }
+    let newNode=null
+    if(!curr.left){
+        newNode = curr.right
+    }else if(!curr.right){
+        newNode = curr.left
+    }else{
+        let temp=curr.right
+        while(temp.left){
+            temp=temp.left 
+        }
+        temp.left=curr.left
+        newNode=curr.right
+    }
+    if(prev.left === curr){
+        prev.left = newNode;
+    }else{
+        prev.right = newNode;
+    }
+    
+    return fakeHead.left
+};
