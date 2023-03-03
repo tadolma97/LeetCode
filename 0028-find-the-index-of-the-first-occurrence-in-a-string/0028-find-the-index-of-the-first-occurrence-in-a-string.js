d8bce7709caa2d926c127b63b06108d9eed549d1
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-03-03 18:51:52 -0500

    Time: 127 ms (5.16%), Space: 49.6 MB (5.28%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let j=0
+    let i=0
+    let count=0
+    while(i<=haystack.length){
+        console.log(i, count, needle.length)
+        if(count===needle.length) return i-needle.length
+        else if(haystack[i]===needle[j]){
+            j++
+            count++
+        }else{
+            j=0
+            i-=count
+            count=0
+        }
+        i++
+    }
+    return -1
+};
