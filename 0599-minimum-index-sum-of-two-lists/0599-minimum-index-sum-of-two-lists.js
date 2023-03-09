0f433d52386763a700c803480bd9ebbca34aaba7
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-03-09 16:22:14 -0500

    Time: 166 ms (15.80%), Space: 56.9 MB (5.18%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    let object={}
+    for(let i=0; i<list1.length;i++){
+        if(list2.includes(list1[i])) object[list1[i]]=i
+    }
+    for(let i=0; i<list2.length;i++){
+        if(list2[i] in object) object[list2[i]]+=i
+    }
+        
+    let arr=Object.values(object)
+    let min=Math.min(...arr)
+    console.log(object, arr, min)
+    return Object.keys(object).filter(key => object[key] === min)
+    
+};
