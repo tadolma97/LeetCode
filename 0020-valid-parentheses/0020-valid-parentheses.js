372a8243426a1fd76fb3712c229df2a1d50ed81b
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-04-18 13:06:51 -0400

    Time: 55 ms (91.80%), Space: 42.4 MB (58.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,18 +3,50 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  var st= []
-  const legend={
-    '(': ')',
-    '{':'}',
-    '[':']'
-  }
-  for(i=0; i<s.length; i++){
-    if(s[i]==='(' || s[i]==='{' || s[i]==='['){
-    st.push(s[i])
-    } else if(legend[st.pop()]!==s[i])
-        return false
-  } 
-  return st.length===0
+//     let old={"{":0,
+//             "[":0,
+//             "(":0}
+//     for(let i=0; i<s.length;i++){
 
+//         if(s[i]==="("){
+//             old["("]=old["("]+1
+//         }else if(s[i]==="{"){
+//             old["{"]=old["{"]+1
+//         }else if(s[i]==="["){
+//             old["["]=old["["]+1
+//         }else if(s[i]===")"){
+//             if(old["("]>0){
+//                 old["("]=old["("]-1
+//             }else return false
+//         }else if(s[i]==="}"){
+//             if(old["{"]>0){
+//                 old["{"]=old["{"]-1
+//             }else return false
+//         }else if(s[i]==="]"){
+//             if(old["["]>0){
+//                 old["["]=old["["]-1
+//             }else return false
+//         }
+//     }
+//     return true
+    let old=[]
+    for(let i=0; i<s.length;i++){  
+        if(s[i]==="{"||s[i]==="["||s[i]==="("){
+            old.push(s[i])
+        }else if(s[i]==="}"){
+            if(old[old.length-1]==="{"){
+                old.pop()
+            }else return false
+        }else if(s[i]===")"){
+            if(old[old.length-1]==="("){
+                old.pop()
+            }else return false
+        }else if(s[i]==="]"){
+            if(old[old.length-1]==="["){
+                old.pop()
+            }else return false
+        }
+    }
+    if(old.length===0) return true
+    else return false
 };
