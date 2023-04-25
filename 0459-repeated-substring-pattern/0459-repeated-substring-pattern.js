01e205456a14fc1e1b69506e45b87082fa621b84
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-04-25 12:38:27 -0400

    Time: 5002 ms (5.11%), Space: 56.5 MB (5.11%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    for(let i=1; i<=(s.length/2); i++){
+        let split=chunk(s, i)
+        let count=0
+        for(let j=0; j<split.length;j++){
+            let first=split[0]
+            if(split[j]===first) count++
+            if(count===split.length) return true
+        }
+    }
+    return false
+
+};
+
+function chunk(str, size) {
+    return str.match(new RegExp('.{1,' + size + '}', 'g'));
+}
