6de43288eeb35a3595a60ab0c3327b99d01d28a7
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-04-25 14:24:27 -0400

    Time: 61 ms (65.83%), Space: 44 MB (25.63%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,20 +3,10 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-
     while(stones.length>1){
-        stones=stones.sort((a,b)=>a-b)
-        let i=stones[stones.length-1]
-        let j=stones[stones.length-2]
-        if(i===j){
-            stones.pop()
-            stones.pop()
-        }else{
-            stones.pop()
-            stones.pop()
-            stones.push(Math.abs(i-j))
-        }
+        stones=stones.sort((a,b)=>b-a)
+        stones[1]=stones[0]-stones[1]
+        stones.shift()
     }
-    if(stones[0])return stones[0]
-    return 0
+    return stones[0]
 };
