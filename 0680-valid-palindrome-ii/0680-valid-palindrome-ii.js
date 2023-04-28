08779581fa05748c6844dec2c4aa8b1658a5d71b
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-04-28 14:59:09 -0400

    Time: 85 ms (47.33%), Space: 50.3 MB (25.61%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left=0
+    let right=s.length-1
+    let count=0
+    return helper(s, left, right, count)
+};
+var helper=function(s, left, right, count){
+    if(count>1) return false
+    if(left>=right) return true
+    if(s[left]===s[right]){
+            left++
+            right--
+            return helper(s, left, right, count)
+    }else {
+        count++
+        if(helper(s, left+1, right, count) || helper(s, left, right-1, count)) return true
+        else return false
+    }
+
+}
