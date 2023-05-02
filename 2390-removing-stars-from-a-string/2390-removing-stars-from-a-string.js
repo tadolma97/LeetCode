d3d232bf75be12512e1749d1456450e1e6d9a7dd
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-05-02 16:33:05 -0400

    Time: 114 ms (49.24%), Space: 58.1 MB (14.22%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    let stack=[]
+    for(let i=0; i<s.length; i++){
+        if(s[i]!=="*") stack.push(s[i])
+        else stack.pop()
+    }
+    return stack.join("")
+};
