fdf0fb13363e5b8fa81c64dda7c09122d38b039c
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-07-25 15:31:40 -0400

    Time: 88 ms (86.69%), Space: 54.7 MB (65.21%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let count=0
+    for(let i=0;i<k;i++){
+        count+=nums[i]
+    }
+    let avg=count/k
+    let max=avg
+    for(let i=k;i<nums.length; i++){
+        count=count-nums[i-k]+nums[i]
+        let avg=count/k
+        if(avg>max) max=avg
+    }
+    return max
+};
