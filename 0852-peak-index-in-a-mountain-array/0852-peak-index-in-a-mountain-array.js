9624e068de990777e04eea270b20d2e6ebe68901
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-07-25 12:27:15 -0400

    Time: 84 ms (17.49%), Space: 51.4 MB (41.77%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    for(let i=0; i<arr.length;i++){
+        let j=i+1
+        if(arr[j]>arr[i]) continue
+        if(arr[j]<arr[i]) return i 
+    }
+};
