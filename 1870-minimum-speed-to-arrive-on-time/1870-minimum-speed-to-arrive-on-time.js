f973a07aba45b70180be0123997a159262995bb6
tadolma97 <102756222+tadolma97@users.noreply.github.com>
2023-08-02 12:10:00 -0400

    Time: 114 ms (92.62%), Space: 54.2 MB (40.55%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.js
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.js
@@ -5,19 +5,21 @@
  */
 var minSpeedOnTime = function(dist, hour) {
     if(dist.length>Math.ceil(hour)) return -1
-    if(dist.reduce((a,b)=>a+b,0)===hour) return 1
-    let right=10e7
+
+    
     let left=1
+    let right=10e7
     while(left<right){
         let mid=Math.floor((left+right)/2)
         let number=0
-        for(let i=0; i<dist.length;i++){
+        for(let i=0;i<dist.length;i++){
             number=Math.ceil(number)
             number+=dist[i]/mid
         }
         if(number>hour) left=mid+1
         else right=mid
-
+            
     }
-    return left
+    return right
+    
 };
